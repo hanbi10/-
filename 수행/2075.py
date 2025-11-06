5e097e4bb4f3d9d8bb06bf31181776d285f096fd
hanbi10 <jeonghanbi10@dgsw.hs.kr>
2025-11-06 14:11:06 +0900

    .

--- 수행/2075.py
+++ 수행/2075.py
@@ -1,10 +1,27 @@
 #배열
 
-N = int(input())  # 몇 번째 큰 수를 찾을지 입력
-numbers = []
+# N = int(input())  # 몇 번째 큰 수를 찾을지 입력
+# numbers = []
 
-for _ in range(N):
-    numbers.extend(map(int, input().split()))
+# for _ in range(N):
+#     numbers.extend(map(int, input().split()))
 
-numbers.sort(reverse=True)
-print(numbers[N-1])
+# numbers.sort(reverse=True)
+# print(numbers[N-1])
+
+N = int(input())
+
+for i in range(N):
+    l = list(map(int,input().split()))
+
+cnt = 0
+max = 0
+
+for i in range(N-1, 0, -1):
+    for j in range(N-1, 0, -1):
+        if l[i][j] > max :
+            max = l[i][j]
+            cnt += 1
+        if cnt == max:
+            print(l[i][j])
+            break
